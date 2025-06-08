76b921f9886818265f7c976a72cc2a96e05acaa6
keewoolee <84774354+keewoolee@users.noreply.github.com>
2025-06-08 23:19:42 +0000

    Deploying to gh-pages from @ CryptoLabInc/ckksorg1659181@09f322b651945fd87f155866d189c935e865fa89 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,29 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/ckksorg1659181/software/";
           },
-        },{id: "news-ckks-org-is-resleeved",
+        },{id: "post-bootstrapping-discrete-data-with-ckks",
+        
+          title: "Bootstrapping Discrete Data with CKKS",
+        
+        description: "TL;DR: Recently, a new paradigm called discrete CKKS, which picks the best aspects of CKKS and other exact schemes has been suggested. To be more specific, it uses CKKS (a.k.a. the approximate homomorphic scheme) to compute over discrete data. In this article, we discuss the recent discrete bootstrapping in BKSS24 specifically designed for discrete CKKS.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/ckksorg1659181/blog/2025/bootstrapping-discrete-data-with-ckks/";
+          
+        },
+      },{id: "post-grafting-improving-performance-and-usability-of-homomorphic-encryption",
+        
+          title: "Grafting: Improving Performance and Usability of Homomorphic Encryption",
+        
+        description: "TL;DR: Grafting is a new approach for managing a CKKS ciphertext modulus. With so-called sprouts, we dedicate a few machine words to scaling and use word-sized primes for the remaining ciphertext modulus improving performance. With universal sprouts, we can represent any bit size up to the word size using powers-of-two and introduce arbitrary scaling for RNS-CKKS easing parameter and circuit design.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/ckksorg1659181/blog/2025/grafting/";
+          
+        },
+      },{id: "news-ckks-org-is-resleeved",
           title: 'CKKS.org is resleeved!',
           description: "",
           section: "News",},{
